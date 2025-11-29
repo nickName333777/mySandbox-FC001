2f03902cb2be622f058b1fa6cea67cd372a1620f
nickName333777 <yypark.rok@gmail.com>
2025-11-29 14:58:14 +0900

    pub->priv

---
+++ chatgptAPI/src/test/java/com/jbj/bsvc/ChatgptApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbj.bsvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatgptApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
